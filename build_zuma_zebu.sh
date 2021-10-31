980ce9ca76805a9e242205140849d9eb949ce172
Hyunki Koo <hyunki00.koo@samsung.com>
2021-10-31 11:26:17 +0900

    add build_zuma_zebu.sh
    build script for build_zuma_zebu is added
    
    Signed-off-by: Hyunki Koo <hyunki00.koo@samsung.com>
    Change-Id: Ide0f809faaa1a216027f63c05201c906ea28aae2

---
+++ build_zuma_zebu.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+
+set -e
+
+: ${OUT_DIR:="out/"}
+export OUT_DIR
+
+LTO=thin \
+BUILD_CONFIG=private/gs-google/device-modules/build.config.zuma_emulator \
+build/build.sh "$@"
+
+BASE_OUT=${OUT_DIR}/
+export DIST_DIR=${DIST_DIR:-${BASE_OUT}/dist/}
+export UNPACK_BOOTIMG_PATH="tools/mkbootimg/unpack_bootimg.py"
+mkdir -p "${DIST_DIR}/zebu/"
+#set -x
+python3 "$UNPACK_BOOTIMG_PATH" --boot_img "${DIST_DIR}/boot.img" --out "${DIST_DIR}/ext_bootimg"
+python3 "$UNPACK_BOOTIMG_PATH" --boot_img "${DIST_DIR}/vendor_boot.img" --out "${DIST_DIR}/ext_vendor_bootimg"
+cat "${DIST_DIR}/ext_vendor_bootimg/vendor_ramdisk00" "${DIST_DIR}/ext_vendor_bootimg/vendor_ramdisk01" "${DIST_DIR}/ext_bootimg/ramdisk" > \
+	      "${DIST_DIR}/zebu/zebu_ramdisk.img"
+#set +x
+cp -v "${DIST_DIR}/Image" "${DIST_DIR}/zebu/Image"
+#cp -v "${DIST_DIR}/ext_vendor_bootimg/dtb" "${DIST_DIR}/zebu/devicetree.dtb"
+#cp -v "${DIST_DIR}/zuma-out.dtb" "${DIST_DIR}/zebu/zuma-emulator.dtb"
+#cp -v "${DIST_DIR}/zuma-out.dtb" "${DIST_DIR}/zebu/zuma-hybrid.dtb"
+cp -v "${DIST_DIR}/zuma-out.dtb" "${DIST_DIR}/zebu/zuma-out.dtb"
