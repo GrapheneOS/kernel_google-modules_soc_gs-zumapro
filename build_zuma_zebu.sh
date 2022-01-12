73f8eea7be6eaad6b804f608b0eb20918213dfa1
Daniel Mentz <danielmentz@google.com>
2022-01-12 11:22:25 -0800

    build_zuma_zebu.sh: Don't explicity invoke python3
    Instead of invoking python3 with the python script as a command line
    argument, let's just depend on the following shebang in
    tools/mkbootimg/unpack_bootimg.py
    
     #!/usr/bin/env python3
    
    Bug: 213797038
    Signed-off-by: Daniel Mentz <danielmentz@google.com>
    Change-Id: Ia25273cef576d817429368fe73559bbe1c79bd1e

--- build_zuma_zebu.sh
+++ build_zuma_zebu.sh
@@ -19,8 +19,8 @@ export DIST_DIR=${DIST_DIR:-${BASE_OUT}/dist/}
 export UNPACK_BOOTIMG_PATH="tools/mkbootimg/unpack_bootimg.py"
 mkdir -p "${DIST_DIR}/zebu/"
 #set -x
-python3 "$UNPACK_BOOTIMG_PATH" --boot_img "${DIST_DIR}/boot.img" --out "${DIST_DIR}/ext_bootimg"
-python3 "$UNPACK_BOOTIMG_PATH" --boot_img "${DIST_DIR}/vendor_boot.img" --out "${DIST_DIR}/ext_vendor_bootimg"
+"$UNPACK_BOOTIMG_PATH" --boot_img "${DIST_DIR}/boot.img" --out "${DIST_DIR}/ext_bootimg"
+"$UNPACK_BOOTIMG_PATH" --boot_img "${DIST_DIR}/vendor_boot.img" --out "${DIST_DIR}/ext_vendor_bootimg"
 cat "${DIST_DIR}/ext_vendor_bootimg/vendor_ramdisk00" "${DIST_DIR}/ext_vendor_bootimg/vendor_ramdisk01" "${DIST_DIR}/ext_bootimg/ramdisk" > \
 	      "${DIST_DIR}/zebu/zebu_ramdisk.img"
 #set +x
