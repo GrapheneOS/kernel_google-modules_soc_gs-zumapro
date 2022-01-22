65cccd5a97b984ce8f4b126c142ca0da30565873
Carlos Llamas <cmllamas@google.com>
2022-01-21 16:29:59 -0800

    usb: typec: tcpci_max77759: stub for tcpm_is_debouncing()
    tcpm_is_debouncing() was removed from android-mainline in commit
    3231c334dde8d ("Revert "ANDROID: usb: typec: tcpm: Add chip level
    callback to check for contaminant""). So, this patch creates a stub for
    the missing api while we wait for the change to be sent upstream.
    
    Bug: 215766959
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Change-Id: I6ae763450ba149fef898cdb93535f8fbedc3177e
    (cherry picked from commit 26f1a6d6f13022ab8bc131cc39930265d88190e3)

--- drivers/usb/typec/tcpm/google/tcpci_max77759.c
+++ drivers/usb/typec/tcpm/google/tcpci_max77759.c
@@ -1070,6 +1070,13 @@ static void max77759_cache_cc(struct max77759_plat *chip)
 	chip->cc2 = cc2;
 }
 
+/**
+ * The patch introducing tcpm_is_debouncing() was reverted in android-mainline
+ * (See aosp/1911031). While we wait for such patch to be sent upstream let's
+ * stub out tcpm_is_debouncing() here. TODO(b/215766959): revert this hack.
+ **/
+static inline bool tcpm_is_debouncing(struct tcpm_port *tcpm) { return false; }
+
 static irqreturn_t _max77759_irq(struct max77759_plat *chip, u16 status,
 				 struct logbuffer *log)
 {
