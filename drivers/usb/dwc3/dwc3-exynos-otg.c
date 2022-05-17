kerneltoast/android_kernel_google_gs201

usb: dwc3: move the operation for burst mode setting
There is an API which is used to setup dwc3 for host mode,
so move the operation for disabling INCR undefined length
burst mode into it.

Bug: 231409025
Test: host mode works
Signed-off-by: Puma Hsu <pumahsu@google.com>
Change-Id: Id5b17f7eb5f068c29145328f74a1b82e64804a89

drivers/usb/dwc3/dwc3-exynos-otg.c
@@ -174,6 +174,11 @@ static void dwc3_otg_set_host_mode(struct dwc3_otg *dotg)
 		reg &= ~DWC3_OTG_OCTL_PERIMODE;
 		dwc3_writel(dotg->regs, DWC3_OCTL, reg);
 	} else {
+		/* Disable undefined length burst mode */
+		reg = dwc3_readl(dwc->regs, DWC3_GSBUSCFG0);
+		reg &= ~(DWC3_GSBUSCFG0_INCRBRSTEN);
+		dwc3_writel(dwc->regs, DWC3_GSBUSCFG0, reg);
+
 		dwc3_otg_set_mode(dwc, DWC3_GCTL_PRTCAP_HOST);
 	}
 }
@@ -380,7 +385,6 @@ static int dwc3_otg_start_host(struct otg_fsm *fsm, int on)
 	int ret = 0;
 	int ret1 = -1;
 	int wait_counter = 0;
-	u32 reg;
 
 	__pm_stay_awake(dotg->wakelock);
 
@@ -420,11 +424,6 @@ static int dwc3_otg_start_host(struct otg_fsm *fsm, int on)
 					__func__);
 			goto err1;
 		}
-
-		/* Disable undefined length burst mode */
-		reg = dwc3_readl(dwc->regs, DWC3_GSBUSCFG0);
-		reg &= ~(DWC3_GSBUSCFG0_INCRBRSTEN);
-		dwc3_writel(dwc->regs, DWC3_GSBUSCFG0, reg);
 
 		/* To ignore gadget suspend/resume on host l2 suspend */
 		exynos->dwc->current_dr_role = DWC3_EXYNOS_IGNORE_CORE_OPS;

GrapheneOS/kernel_google-modules_soc_gs-zumapro

(cherry picked from commit 371e579e5d6be96237681a311117087f79e552b3)
Signed-off-by: Will McVicker <willmcvicker@google.com>
[willmcvicker: resolve merge conflicts]

drivers/usb/dwc3/dwc3-exynos-otg.c
@@ -173,6 +173,11 @@ static void dwc3_otg_set_host_mode(struct dwc3_otg *dotg)
 		reg &= ~DWC3_OTG_OCTL_PERIMODE;
 		dwc3_exynos_writel(dotg->regs, DWC3_OCTL, reg);
 	} else {
+		/* Disable undefined length burst mode */
+		reg = dwc3_exynos_readl(dwc->regs, DWC3_GSBUSCFG0);
+		reg &= ~(DWC3_GSBUSCFG0_INCRBRSTEN);
+		dwc3_exynos_writel(dwc->regs, DWC3_GSBUSCFG0, reg);
+
 		dwc3_otg_set_mode(dwc, DWC3_GCTL_PRTCAP_HOST);
 	}
 }
@@ -379,7 +384,6 @@ static int dwc3_otg_start_host(struct otg_fsm *fsm, int on)
 	int ret = 0;
 	int ret1 = -1;
 	int wait_counter = 0;
-	u32 reg;
 
 	__pm_stay_awake(dotg->wakelock);
 
@@ -419,11 +423,6 @@ static int dwc3_otg_start_host(struct otg_fsm *fsm, int on)
 					__func__);
 			goto err1;
 		}
-
-		/* Disable undefined length burst mode */
-		reg = dwc3_exynos_readl(dwc->regs, DWC3_GSBUSCFG0);
-		reg &= ~(DWC3_GSBUSCFG0_INCRBRSTEN);
-		dwc3_exynos_writel(dwc->regs, DWC3_GSBUSCFG0, reg);
 
 		/* To ignore gadget suspend/resume on host l2 suspend */
 		exynos->dwc->current_dr_role = DWC3_EXYNOS_IGNORE_CORE_OPS;