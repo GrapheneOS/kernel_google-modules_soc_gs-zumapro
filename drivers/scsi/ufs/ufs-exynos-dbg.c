e19ba18aba7b45191529c54fba8428441673eb7b
Kiwoong Kim <kwmad.kim@samsung.com>
2022-06-02 15:51:56 +0900

    ufs: add ufs-exynos-dbg.h
    To put ufs dump list into a header file per project,
    we need an additional header file to define some things
    that are used in the ufs dump list header file.
    
    Change-Id: I521fe9fe5db92f93f39f97a69ec3f44a6a03b838
    Signed-off-by: Kiwoong Kim <kwmad.kim@samsung.com>

--- drivers/scsi/ufs/ufs-exynos-dbg.c
+++ drivers/scsi/ufs/ufs-exynos-dbg.c
@@ -23,50 +23,7 @@
 #include "ufshcd.h"
 #include "ufs-vs-mmio.h"
 #include "ufs-vs-regs.h"
-
-enum {
-	LOG_STD_HCI_SFR = 0xFFFFFFF0,
-	LOG_VS_HCI_SFR,
-	LOG_FMP_SFR,
-	LOG_UNIPRO_SFR,
-	LOG_PMA_SFR,
-	LOG_INVALID,
-};
-
-enum {
-	SFR_VAL_H_0_FIRST = 0,
-	SFR_VAL_H_0,
-	SFR_VAL_NUM,
-};
-
-enum {
-	ATTR_VAL_H_0_L_0_FIRST = 0,
-	ATTR_VAL_H_0_L_1_FIRST,
-	ATTR_VAL_H_0_L_0,
-	ATTR_VAL_H_0_L_1,
-	ATTR_VAL_NUM,
-};
-
-enum {
-	DBG_ATTR_UNIPRO = 0xEFFFFFF0,
-	DBG_ATTR_PCS_CMN,
-	DBG_ATTR_PCS_TX,
-	DBG_ATTR_PCS_RX,
-	DBG_ATTR_INVALID,
-};
-
-struct exynos_ufs_sfr_log {
-	const char *name;
-	const u32 offset;
-	u32 val[SFR_VAL_NUM];
-};
-
-struct exynos_ufs_attr_log {
-	const u32 mib;
-	const u32 offset;
-	/* 0: lane0, 1: lane1 */
-	u32 val[ATTR_VAL_NUM];
-};
+#include "ufs-exynos-dbg.h"
 
 /* Structure for ufs cmd logging */
 #define MAX_CMD_LOGS    32
