e89eec844afa567cbdee0ae9272921bf33b9ef7a
Kiwoong Kim <kwmad.kim@samsung.com>
2022-06-09 13:25:26 +0900

    ufs: add a directory for zuma project
    It contains ufs cal and ufs dump list.
    
    Change-Id: Ie1c221f2d87185de25f7da364c3baf07bf0f2317
    Signed-off-by: Kiwoong Kim <kwmad.kim@samsung.com>

--- drivers/scsi/ufs/ufs-cal-if.h
+++ drivers/scsi/ufs/ufs-cal-if.h
@@ -6,8 +6,10 @@
 #ifndef _UFS_CAL_IF_H
 #define _UFS_CAL_IF_H
 
-#ifdef CONFIG_SOC_GS101
+#if defined(CONFIG_SOC_GS101)
 #include "gs101/ufs-cal-if.h"
+#elif defined(CONFIG_SOC_ZUMA)
+#include "zuma/ufs-cal-if.h"
 #endif
 
 #endif /*_UFS_CAL_IF_H */
--- drivers/scsi/ufs/ufs-dump.h
+++ drivers/scsi/ufs/ufs-dump.h
@@ -3,13 +3,13 @@
  * Copyright 2021 Google LLC
  */
 
-#ifndef _UFS_VS_MMIO_H
-#define _UFS_VS_MMIO_H
+#ifndef _UFS_DUMP_CMN_H
+#define _UFS_DUMP_CMN_H
 
 #if defined(CONFIG_SOC_GS101)
-#include "gs101/ufs-vs-mmio.h"
+#include "gs101/ufs-dump.h"
 #elif defined(CONFIG_SOC_ZUMA)
-#include "zuma/ufs-vs-mmio.h"
+#include "zuma/ufs-dump.h"
 #endif
 
-#endif /*_UFS_VS_MMIO_H */
+#endif /*_UFS_DUMP_CMN_H */
