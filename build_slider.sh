33fe7558ce6df393cfafc2aa095076107d064389
Will McVicker <willmcvicker@google.com>
2022-06-24 17:00:58 -0700

    build_slider.sh: use generated GKI boot.img
    * Update build.config.gs101 to not generate a boot.img. We can use the
      one created by BUILD_GKI_ARTIFACTS=1
    * Add option to build_slider.sh to skip compiling the vendor modules.
      This lets you re-compile only the GKI kernel and boot.img.
    * Update build.sh path to use full "build/kernel/build.sh".
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: I22303efd11d79f851f3bb012de8052fc504fe4df

--- build_slider.sh
+++ build_slider.sh
@@ -11,11 +11,28 @@ function exit_if_error {
 export LTO=${LTO:-thin}
 export BUILD_CONFIG=private/google-modules/soc/gs/build.config.slider
 
-if [ -z "${GKI_PREBUILTS_DIR}" ]; then
+if [ -n "${SKIP_VENDOR_MODULES}" ] &&
+   [ ! -f "${OUT_DIR:-out/android-mainline}/dist/boot.img" ]; then
+  echo "WARNING: no \$DIST_DIR/boot.img found. Running full mixed build." >&2
+  SKIP_VENDOR_MODULES=
+fi
+
+if [ -n "${SKIP_VENDOR_MODULES}" ]; then
+  ORIG_OUT_DIR=${OUT_DIR:-out/android-mainline}
+  export OUT_DIR=${ORIG_OUT_DIR}/gki_kernel
+
+  BUILD_CONFIG=common/build.config.gki.aarch64 \
+    build/kernel/build.sh "$@"
+elif [ -z "${GKI_PREBUILTS_DIR}" ]; then
   GKI_BUILD_CONFIG=common/build.config.gki.aarch64 \
-    build/build.sh "$@"
+    build/kernel/build.sh "$@"
 else
-  build/build.sh "$@"
+  build/kernel/build.sh "$@"
 fi
 
 exit_if_error $? "Failed to create mixed build"
+
+# Update the boot.img in the main dist dir
+if [ -n "${SKIP_VENDOR_MODULES}" ]; then
+  cp ${OUT_DIR}/dist/boot.img ${ORIG_OUT_DIR}/dist/boot.img
+fi
