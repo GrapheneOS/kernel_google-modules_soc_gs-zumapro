0ca633e8b9f6b570af22fb04b032274a7c2de481
Ray Hsu <raylinhsu@google.com>
2022-10-11 13:16:04 +0000

    soc: gs-chipid: Support different size of raw_str for different chips
    GS101, GS201 and ZUMA have different size of data reported in chip-id/raw_str.
    
    Bug: 245428164
    Change-Id: Ie6a3ad96816ca0ae6541da7fb7db63a1fcbba310

--- drivers/soc/google/gs-chipid.c
+++ drivers/soc/google/gs-chipid.c
@@ -24,13 +24,14 @@ struct gs_chipid_variant {
 	int sub_rev_bit;
 };
 
-#define RAW_HEX_STR_SIZE 132
+#define RAW_HEX_STR_SIZE 172
 #define AP_HW_TUNE_HEX_STR_SIZE 64
 #define AP_HW_TUNE_HEX_ARRAY_SIZE 32
 #define ASV_TBL_HEX_STR_SIZE 128
 #define HPM_ASV_HEX_STR_SIZE 128
 #define GS101_HPM_ASV_END_ADDR 0xA024
-#define HPM_ASV_END_ADDR 0xA02C
+#define GS201_HPM_ASV_END_ADDR 0xA02C
+#define HPM_ASV_END_ADDR 0xA040
 
 static void gs_chipid_get_asv_tbl_str(void __iomem *reg);
 static void gs_chipid_get_hpm_asv_str(void __iomem *reg);
@@ -61,6 +62,7 @@ struct gs_chipid_info {
 };
 
 #define GS101_SOC_ID		0x09845000
+#define GS201_SOC_ID		0x09855000
 #define ZUMA_SOC_ID		0x09865000
 #define SOC_MASK		0xFFFFF000
 #define SOC_MASK_V2		0x00FFFFFF
@@ -84,6 +86,9 @@ static const char *product_id_to_name(unsigned int product_id)
 	case GS101_SOC_ID:
 		soc_name = "GS101";
 		break;
+	case GS201_SOC_ID:
+		soc_name = "GS201";
+		break;
 	case ZUMA_SOC_ID:
 		soc_name = "ZUMA";
 		break;
@@ -94,6 +99,14 @@ static const char *product_id_to_name(unsigned int product_id)
 }
 
 static const struct gs_chipid_variant drv_data_gs101 = {
+	.product_ver = 1,
+	.unique_id_reg = 0x04,
+	.rev_reg = 0x10,
+	.main_rev_bit = 0,
+	.sub_rev_bit = 16,
+};
+
+static const struct gs_chipid_variant drv_data_gs201 = {
 	.product_ver = 1,
 	.unique_id_reg = 0x04,
 	.rev_reg = 0x10,
@@ -343,17 +356,22 @@ static void gs_chipid_get_raw_str(void __iomem *reg)
 				     str_buf_size - str_pos,
 				     "%02x", val);
 	}
+
 	if (gs_soc_info.product_id == GS101_SOC_ID) {
 		addr_end = GS101_HPM_ASV_END_ADDR;
+	} else if (gs_soc_info.product_id == GS201_SOC_ID) {
+		addr_end = GS201_HPM_ASV_END_ADDR;
 	} else {
 		addr_end = HPM_ASV_END_ADDR;
 	}
+
 	for (addr = 0xA000; addr < addr_end; addr++) {
 		val = readb_relaxed(reg + addr);
 		str_pos += scnprintf(gs_soc_info.raw_str + str_pos,
 				     str_buf_size - str_pos,
 				     "%02x", val);
 	}
+
 	for (addr = 0x9000; addr < 0x9010; addr++) {
 		val = readb_relaxed(reg + addr);
 		str_pos += scnprintf(gs_soc_info.raw_str + str_pos,
@@ -424,11 +442,15 @@ static const struct of_device_id of_gs_chipid_ids[] = {
 	{
 	 .compatible = "google,gs101-chipid",
 	 .data = &drv_data_gs101,
-	 },
-	 {
+	},
+	{
+	 .compatible = "google,gs201-chipid",
+	 .data = &drv_data_gs201,
+	},
+	{
 	 .compatible = "google,zuma-chipid",
 	 .data = &drv_data_zuma,
-	 },
+	},
 	{},
 };
 
