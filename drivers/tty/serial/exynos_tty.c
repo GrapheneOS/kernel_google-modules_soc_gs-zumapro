306e35c92626b530a7bad467abee9c542836d17f
Will McVicker <willmcvicker@google.com>
2022-10-11 11:28:56 -0700

    serial: exynos: update set_termios API
    The upstream commit bec5b814d46c ("serial: Make ->set_termios() old
    ktermios const") changes the upstream set_termios() API. So update this
    driver accordingly.
    
    Bug: 253052273
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: If7c76d4deb89332bd35edb0d88cbfeb6596eaa90

--- drivers/tty/serial/exynos_tty.c
+++ drivers/tty/serial/exynos_tty.c
@@ -1787,7 +1787,7 @@ static u16 udivslot_table[16] = {
 
 static void exynos_serial_set_termios(struct uart_port *port,
 				      struct ktermios *termios,
-				      struct ktermios *old)
+				      const struct ktermios *old)
 {
 	struct s3c2410_uartcfg *cfg = exynos_port_to_cfg(port);
 	struct exynos_uart_port *ourport = to_ourport(port);
