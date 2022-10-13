db88af4aaacfaf08180c0d3cc03eea8c5f7e12dc
Will McVicker <willmcvicker@google.com>
2022-10-13 08:55:12 -0700

    soc: google: dss: switch from PF_FROZEN to TASK_FROZEN
    The upstream commit f5d39b020809 ("freezer,sched: Rewrite core freezer
    logic") dropped the PF_FROZEN define in favor of drivers using the
    TASK_FROZEN define. So update this driver accordingly.
    
    Bug: 253446924
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: I28da5fc48ca9eeb8400a8ef894f28402d3f4c4bf

--- drivers/soc/google/debug/debug-snapshot-utils.c
+++ drivers/soc/google/debug/debug-snapshot-utils.c
@@ -272,7 +272,7 @@ static void dbg_snapshot_dump_one_task_info(struct task_struct *tsk, bool is_mai
 	unsigned char idx = 0;
 	unsigned long state, pc = 0;
 
-	if ((!tsk) || !try_get_task_stack(tsk) || (tsk->flags & PF_FROZEN) ||
+	if ((!tsk) || !try_get_task_stack(tsk) || (tsk->__state & TASK_FROZEN) ||
 			!(tsk->__state == TASK_RUNNING ||
 				tsk->__state == TASK_UNINTERRUPTIBLE ||
 				tsk->__state == TASK_KILLABLE))
