1fe25baf707aaf8ae5586248b8c64949024a8420
Qais Yousef <qyousef@google.com>
2023-02-22 18:00:35 +0000

    vh: sched: cpufreq_gov: cap iowait boost by uclamp_max
    Which is a backport of upstream fix:
    
    d37aee9018e6 ("sched/uclamp: Fix iowait boost escaping uclamp restriction")
    
    Bug: 261695814
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: Ibe8175edb9dea35e325f1a6f4306885ab8b6b28a

--- drivers/soc/google/vh/kernel/sched/cpufreq_gov.c
+++ drivers/soc/google/vh/kernel/sched/cpufreq_gov.c
@@ -709,7 +709,10 @@ static unsigned long sugov_iowait_apply(struct sugov_cpu *sg_cpu, u64 time,
 	 * into the same scale so we can compare.
 	 */
 	boost = (sg_cpu->iowait_boost * max) >> SCHED_CAPACITY_SHIFT;
-	return max(boost, util);
+	boost = max(boost, util);
+	boost = uclamp_rq_util_with(cpu_rq(sg_cpu->cpu), boost, NULL);
+
+	return boost;
 }
 
 #if IS_ENABLED(USE_UPDATE_SINGLE)
