e324ec77b73314e4d1adbd9840ff3499b7733885
Badhri Jagan Sridharan <badhri@google.com>
2023-03-05 09:08:33 +0000

    tcpci_max77759: Add logic to report AICL activations
    When the user plugs in either a high IR drop cable or a
    source with poor voltage regulation, the current drawn by the port
    is limited as the hardware enables AICL loop. Report these
    as COMPLIANCE_WARNING_OTHER
    
    Bug: 273366006
    Change-Id: Id99d27ee9f56910ac087d33971102cd03023bf94
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    (cherry picked from commit 071fb1ddca7a8bbfa250f7be3c1062a2e4e0283e)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/usb/typec/tcpm/google/tcpci_max77759.h
+++ drivers/usb/typec/tcpm/google/tcpci_max77759.h
@@ -16,6 +16,7 @@
 #include <linux/usb/role.h>
 #include <linux/usb/typec_mux.h>
 #include <linux/usb/tcpci.h>
+#include <misc/gvotable.h>
 
 #include "usb_psy.h"
 
@@ -163,6 +164,9 @@ struct max77759_plat {
 	 */
 	bool first_rp_missing_timeout;
 
+	/* Signal from charger when AICL is active. */
+	struct gvotable_election *aicl_active_el;
+
 	/* EXT_BST_EN exposed as GPIO */
 #ifdef CONFIG_GPIOLIB
 	struct gpio_chip gpio;
@@ -221,6 +225,7 @@ void enable_data_path_locked(struct max77759_plat *chip);
 void data_alt_path_active(struct max77759_plat *chip, bool active);
 void register_data_active_callback(void (*callback)(void *data_active_payload), void *data);
 
+/* AICL_OK is tracked with COMPLIANCE_WARNING_OTHER */
 #define COMPLIANCE_WARNING_OTHER 0
 #define COMPLIANCE_WARNING_DEBUG_ACCESSORY 1
 #define COMPLIANCE_WARNING_BC12 2
