1a2000309e7dda4d25ecddff5a02a1383363349e
Samuel Gosselin <sgosselin@google.com>
2023-04-14 03:39:31 +0000

    misc: re-align the cloudripper build script with slider.
    Change-Id: Ie8c821e57f502de433a1dec3035ff9d5c3905e19

--- build_cloudripper.sh
+++ build_cloudripper.sh
@@ -1,4 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
-DEVICE_KERNEL_BUILD_CONFIG=private/gs-google/build.config.cloudripper \
-private/gs-google/build_slider.sh "$@"
+
+function exit_if_error {
+  if [ $1 -ne 0 ]; then
+    echo "ERROR: $2: retval=$1" >&2
+    exit $1
+  fi
+}
+
+export LTO=${LTO:-thin}
+export BUILD_CONFIG=private/google-modules/soc/gs/build.config.cloudripper
+export KLEAF_SUPPRESS_BUILD_SH_DEPRECATION_WARNING=1
+
+if [ -z "${GKI_PREBUILTS_DIR}" ]; then
+  GKI_BUILD_CONFIG=common/build.config.gki.aarch64 \
+    build/build.sh "$@"
+else
+  build/build.sh "$@"
+fi
+
+exit_if_error $? "Failed to create mixed build"
