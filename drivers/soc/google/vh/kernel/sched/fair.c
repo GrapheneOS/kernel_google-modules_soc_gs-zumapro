747f110210b77724df19b0777fae637e1fe99b89
Qais Yousef <qyousef@google.com>
2023-04-28 17:55:03 +0000

    vh: sched: Fix iowait boost updates
    Commit 44070558c0071 ("vh: sched: Block cpufreq updates until uclamp
    filters are applied") accidently broke iowait boost as it forgot to
    pass on the flag when we temporarily block cpufreq updates. Which meant
    when we go back and resume, the iowait update isn't applied causing some
    regression on workloads that are sensitive to this parameter.
    
    Rectify it by passing the iowait boost flag so that we can remember to
    apply the boost when we resume the cpufreq updates.
    
    Bug: 279805535
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: Id1949cb88e636cf2e916fe0be5866c99c84dfead

--- drivers/soc/google/vh/kernel/sched/fair.c
+++ drivers/soc/google/vh/kernel/sched/fair.c
@@ -1978,7 +1978,12 @@ out:
 void rvh_set_iowait_pixel_mod(void *data, struct task_struct *p, struct rq *rq,
 			      int *should_iowait_boost)
 {
+	unsigned int flags = SCHED_PIXEL_BLOCK_UPDATES;
+
 	*should_iowait_boost = p->in_iowait && uclamp_boosted(p);
+
+	if (*should_iowait_boost)
+		flags |= SCHED_CPUFREQ_IOWAIT;
 
 	/*
 	 * Tell sched pixel to ignore cpufreq updates. this happens at
@@ -1992,7 +1997,7 @@ void rvh_set_iowait_pixel_mod(void *data, struct task_struct *p, struct rq *rq,
 	 * - this strategic block will ensure all subsequent requests are
 	 *   ignored.
 	 */
-	cpufreq_update_util(task_rq(p), SCHED_PIXEL_BLOCK_UPDATES);
+	cpufreq_update_util(task_rq(p), flags);
 }
 
 void rvh_cpu_overutilized_pixel_mod(void *data, int cpu, int *overutilized)
