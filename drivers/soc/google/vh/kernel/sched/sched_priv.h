ee0ee0bb99ea98a5eb4234aece2276a3c9c60782
Rick Yiu <rickyiu@google.com>
2023-05-01 13:53:38 +0000

    vh: sched: Refine per-group tunables for uclamp on nice
    1. clamp uclamp on nice values with other uclamp values
    2. assure the correct order of prio for each levels
    3. change the default prio values
    
    Bug: 254737034
    Change-Id: I637da2cc9cdc722643f6dad179b1fe155b3ea293
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- drivers/soc/google/vh/kernel/sched/sched_priv.h
+++ drivers/soc/google/vh/kernel/sched/sched_priv.h
@@ -13,8 +13,9 @@
 #define DEF_UTIL_THRESHOLD  1280
 #define DEF_UTIL_POST_INIT_SCALE  512
 #define C1_EXIT_LATENCY     1
-#define PREFER_IDLE_PRIO_HIGH 110
-#define PRIO_BACKGROUND     130
+#define THREAD_PRIORITY_TOP_APP_BOOST 110
+#define THREAD_PRIORITY_BACKGROUND    130
+#define THREAD_PRIORITY_LOWEST        139
 /*
  * For cpu running normal tasks, its uclamp.min will be 0 and uclamp.max will be 1024,
  * and the sum will be 1024. We use this as index that cpu is not running important tasks.
