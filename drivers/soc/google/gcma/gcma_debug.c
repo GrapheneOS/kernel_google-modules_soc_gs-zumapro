98139133b9168bb760db4c9c96a4135ba51418c4
Minchan Kim <minchan@google.com>
2023-05-23 14:58:55 -0700

    gcma: force eviction for testing
    The eviction is triggered only when entire gcma ranges are full.
    It makes testing harder to trigger so add artificial knob to
    trigger the eviction.
    
    Bug: 283533468
    Change-Id: I140dd13ddbcf55b993a9835d3829b48559b8b12b
    Signed-off-by: Minchan Kim <minchan@google.com>

--- drivers/soc/google/gcma/gcma_debug.c
+++ drivers/soc/google/gcma/gcma_debug.c
@@ -1,12 +1,23 @@
 #include <linux/types.h>
 #include <linux/debugfs.h>
 
+#include "gcma_core.h"
+
 static bool workingset = true;
 
 bool workingset_filter_enabled(void)
 {
 	return workingset;
 }
+
+static int gcma_evict_write(void *data, u64 val)
+{
+	unsigned long nr_pages = val;
+
+	evict_gcma_lru_pages(nr_pages);
+	return 0;
+}
+DEFINE_DEBUGFS_ATTRIBUTE(gcma_evict_fops, NULL, gcma_evict_write, "%llu\n");
 
 int __init gcma_debugfs_init(void)
 {
@@ -17,6 +28,7 @@ int __init gcma_debugfs_init(void)
 		return -ENOMEM;
 
 	debugfs_create_bool("workingset", 0644, gcma_debugfs_root, &workingset);
+	debugfs_create_file("evict", 0200, gcma_debugfs_root, NULL, &gcma_evict_fops);
 
 	return 0;
 }
