1c5865abf83145de04df1df41fd58fabafe2b7db
Chungkai Mei <chungkai@google.com>
2023-06-19 09:34:32 +0000

    vh: sched: Only pull latency sensitive tasks in newidle_balance
    When we detect there are multiple latency sensitive tasks stuck on the
    same CPU, the logic to pull could pull some other type of tasks beside
    latency sensitive (boosted tasks). Fix the logic to only pull latency
    sensitive ones.
    
    Bug: 281715990
    Bug: 287641634
    Change-Id: I59744379622bd61bc7f69f7674388e00183b33fd
    Signed-off-by: Chungkai Mei <chungkai@google.com>
    Signed-off-by: Qais Yousef <qyousef@google.com>

--- drivers/soc/google/vh/kernel/sched/fair.c
+++ drivers/soc/google/vh/kernel/sched/fair.c
@@ -2534,15 +2534,14 @@ void rvh_setscheduler_pixel_mod(void *data, struct task_struct *p)
 
 static struct task_struct *detach_important_task(struct rq *src_rq, int dst_cpu)
 {
-	struct task_struct *p = NULL, *best_task = NULL, *backup = NULL,
-		*backup_ui = NULL, *backup_unfit = NULL;
+	struct task_struct *p;
 
 	lockdep_assert_held(&src_rq->__lock);
 
 	rcu_read_lock();
 
 	list_for_each_entry_reverse(p, &src_rq->cfs_tasks, se.group_node) {
-		bool is_ui = false, is_boost = false;
+		struct vendor_task_struct *vp = get_vendor_task_struct(p);
 
 		if (!cpumask_test_cpu(dst_cpu, p->cpus_ptr))
 			continue;
@@ -2550,66 +2549,22 @@ static struct task_struct *detach_important_task(struct rq *src_rq, int dst_cpu)
 		if (task_on_cpu(src_rq, p))
 			continue;
 
-		if (!get_prefer_idle(p))
-			continue;
-
-		if (get_uclamp_fork_reset(p, true))
-			is_ui = true;
-		else if (uclamp_eff_value_pixel_mod(p, UCLAMP_MIN) > 0)
-			is_boost = true;
-
-		if (!is_ui && !is_boost)
-			continue;
-
-		if (task_fits_capacity(p, dst_cpu, false)) {
-			if (!task_fits_capacity(p, src_rq->cpu, false)) {
-				// if task is fit for new cpu but not old cpu
-				// stop if we found an ADPF UI task
-				// use it as backup if we found a boost task
-				if (is_ui) {
-					best_task = p;
-					break;
-				}
-
-				backup = p;
-			} else {
-				if (is_ui) {
-					backup_ui = p;
-					continue;
-				}
-
-				if (!backup)
-					backup = p;
-			}
-		} else {
-			// if new idle is not capable, use it as backup but not for UI task.
-			if (!is_ui)
-				backup_unfit = p;
+		/* if latency sensitive task and dst cpu fits, pull it */
+		if (vp && vp->uclamp_fork_reset &&
+		    task_fits_capacity(p, dst_cpu, false)) {
+			goto found;
 		}
 	}
 
-	if (best_task)
-		p = best_task;
-	else if (backup_ui)
-		p = backup_ui;
-	else if (backup)
-		p = backup;
-	else if (backup_unfit)
-		p = backup_unfit;
-	else
-		p = NULL;
+	p = NULL;
+	goto out;
 
-	if (p) {
-		/* detach_task */
-		deactivate_task(src_rq, p, DEQUEUE_NOCLOCK);
-		set_task_cpu(p, dst_cpu);
-
-		if (backup_unfit)
-			cpu_rq(dst_cpu)->misfit_task_load = p->se.avg.load_avg;
-		else
-			cpu_rq(dst_cpu)->misfit_task_load = 0;
-	}
-
+found:
+	/* detach_task */
+	deactivate_task(src_rq, p, DEQUEUE_NOCLOCK);
+	set_task_cpu(p, dst_cpu);
+ 
+out:
 	rcu_read_unlock();
 	return p;
 }
@@ -2633,6 +2588,7 @@ void sched_newidle_balance_pixel_mod(void *data, struct rq *this_rq, struct rq_f
 	if (SCHED_WARN_ON(atomic_read(&this_vrq->num_adpf_tasks)))
 		atomic_set(&this_vrq->num_adpf_tasks, 0);
 
+	this_rq->misfit_task_load = 0;
 
 	if (!vendor_sched_idle_balancer)
 		return;
