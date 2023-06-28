84fc71f983eb6a79a40b1ae83547b736469373f2
Ram Chandrasekar <rchandrasekar@google.com>
2023-06-28 16:04:22 -0600

    drivers: thermal: gs_tmu: Register cooling device
    Register cooling devices for each Tj loop. This helps userspace
    to fetch the current Tj cdev vote.
    
    Bug: 287278792
    Test: Verified BIG cdev state changed with geekbench
    
    Change-Id: I843aa48372c44f3d2c0bb6304106d681c20273ec
    Signed-off-by: Ram Chandrasekar <rchandrasekar@google.com>

--- drivers/thermal/samsung/gs_tmu_v3.c
+++ drivers/thermal/samsung/gs_tmu_v3.c
@@ -23,6 +23,7 @@
 #include <linux/slab.h>
 #include <linux/debugfs.h>
 #include <uapi/linux/sched/types.h>
+#include <uapi/linux/thermal.h>
 #include <soc/google/bcl.h>
 #include <soc/google/gs_tmu_v3.h>
 #include <soc/google/tmu.h>
@@ -4164,6 +4165,76 @@ static void pause_stats_setup(struct gs_tmu_data *data)
 	spin_lock_init(&stats->lock);
 }
 
+/**
+ * tmu_tj_throttle_get_cur_state - Get the current Tj throttling vote.
+ * @cdev: the thermal cooling device.
+ * @state: the target state will be populated in this variable.
+ *
+ * This function fetches the current Tj throttling vote.
+ *
+ * Return: 0 when the Tj vote is successfully fetched otherwise -EIO is
+ * returned.
+ */
+static int tmu_tj_throttle_get_cur_state(struct thermal_cooling_device *cdev,
+					 unsigned long *state)
+{
+	struct gs_tmu_data *tmu_data = cdev->devdata;
+	struct curr_state curr_state;
+
+	if (ACPM_BUF_VER == EXPECT_BUF_VER &&
+	    get_curr_state_from_acpm(acpm_gov_common.sm_base, tmu_data->id, &curr_state))
+		*state = curr_state.cdev_state;
+	else
+		return -EIO;
+
+	return 0;
+}
+
+/**
+ * tmu_tj_throttle_set_cur_state - A NOP.
+ * @cdev: the thermal cooling device.
+ * @state: the target state.
+ *
+ * Tj current state can't be changed from HLOS. So this callback will be a NOP.
+ * This callback will never be called, because the max_state is returned as 0
+ * and hence thermal core will return -EIVAL for any request > max_state.
+ *
+ * Return: Always 0.
+ */
+static int tmu_tj_throttle_set_cur_state(struct thermal_cooling_device *cdev,
+					 unsigned long state)
+{
+	return 0;
+}
+
+/**
+ * tmu_tj_throttle_get_max_state - A NOP, returns 0 always.
+ * @cdev: the thermal cooling device.
+ * @state: the maximum possible state will be populated in this variable.
+ *
+ * This function populates the max possible cooling device state. But this
+ * callback returns 0 always.
+ *
+ * Return: Always 0.
+ */
+static int tmu_tj_throttle_get_max_state(struct thermal_cooling_device *cdev,
+					 unsigned long *state)
+{
+	/*
+	 * TODO (rchandrasekar): These cooling device should be registered by
+	 * their respective drivers and hence they will have the information
+	 * about the max state.
+	 */
+	*state = 0;
+	return 0;
+}
+
+static struct thermal_cooling_device_ops tmu_tj_cooling_ops = {
+	.get_max_state = tmu_tj_throttle_get_max_state,
+	.get_cur_state = tmu_tj_throttle_get_cur_state,
+	.set_cur_state = tmu_tj_throttle_set_cur_state,
+};
+
 #if IS_ENABLED(CONFIG_EXYNOS_ACPM_THERMAL)
 static u8 tmu_id;
 static u16 tmu_reg_offset;
@@ -5031,6 +5102,7 @@ static int gs_tmu_probe(struct platform_device *pdev)
 		.get_stats = thermal_metrics_get_tr_stats,
 		.reset_stats = thermal_metrics_reset_stats,
 	};
+	char cdev_buf[THERMAL_NAME_LENGTH] = "";
 
 	data = devm_kzalloc(&pdev->dev, sizeof(struct gs_tmu_data), GFP_KERNEL);
 	if (!data)
@@ -5305,6 +5377,11 @@ static int gs_tmu_probe(struct platform_device *pdev)
 	spin_unlock(&dev_list_spinlock);
 
 	register_tz_id_ignore_genl(data->tzd->id);
+	/* Register a cooling device to fetch the Tj throttling request. */
+	scnprintf(cdev_buf, THERMAL_NAME_LENGTH, "%s-tj", data->tmu_name);
+	devm_thermal_of_cooling_device_register(
+			&pdev->dev, pdev->dev.of_node, cdev_buf, data,
+			&tmu_tj_cooling_ops);
 
 	return 0;
 
