336f053f767b50dfafe06db462ed5695eedcc4a2
David Chao <davidchao@google.com>
2023-06-28 08:45:15 +0000

    drivers: thermal: tmu: using early_throttling_p instead of pid_err_integral
    Reusing the pid_integral slot to log early_throttling_p
    
    Bug: 279088463
    Test: build, enable acpm PI control, and run trace in both bulk and
    pid_et_p can be parsed.
    
    Change-Id: I114acca98a2eb0cb8942af49f5f80508f1e4bb1a

--- include/trace/events/thermal_exynos.h
+++ include/trace/events/thermal_exynos.h
@@ -143,14 +143,14 @@ TRACE_EVENT(thermal_exynos_power_allocator,
 );
 
 TRACE_EVENT(thermal_exynos_acpm_bulk,
-	TP_PROTO(u8 tz_id, u8 current_temp, u8 ctrl_temp, u8 cdev_state, s32 pid_err_integral, s16 pid_power_range, s16 pid_p, s32 pid_i, s32 k_p, s32 k_i, u64 timestamp),
-	TP_ARGS(tz_id, current_temp, ctrl_temp, cdev_state, pid_err_integral, pid_power_range, pid_p, pid_i, k_p, k_i, timestamp),
+	TP_PROTO(u8 tz_id, u8 current_temp, u8 ctrl_temp, u8 cdev_state, s32 pid_et_p, s16 pid_power_range, s16 pid_p, s32 pid_i, s32 k_p, s32 k_i, u64 timestamp),
+	TP_ARGS(tz_id, current_temp, ctrl_temp, cdev_state, pid_et_p, pid_power_range, pid_p, pid_i, k_p, k_i, timestamp),
 	TP_STRUCT__entry(
 		__field(u8, tz_id)
 		__field(u8, current_temp)
 		__field(u8, ctrl_temp)
 		__field(u8, cdev_state)
-		__field(s32, pid_err_integral)
+		__field(s32, pid_et_p)
 		__field(s16, pid_power_range)
 		__field(s16, pid_p)
 		__field(s32, pid_i)
@@ -163,7 +163,7 @@ TRACE_EVENT(thermal_exynos_acpm_bulk,
 		__entry->current_temp = current_temp;
 		__entry->ctrl_temp = ctrl_temp;
 		__entry->cdev_state = cdev_state;
-		__entry->pid_err_integral = pid_err_integral;
+		__entry->pid_et_p = pid_et_p;
 		__entry->pid_power_range = pid_power_range;
 		__entry->pid_p = pid_p;
 		__entry->pid_i = pid_i;
@@ -172,19 +172,19 @@ TRACE_EVENT(thermal_exynos_acpm_bulk,
 		__entry->timestamp = timestamp;
 	),
 
-	TP_printk("thermal_zone_id=%u current_temperature=%u ctrl_temp=%u cdev_state=%u pid_err_integral=%d pid_power_range=%d pid_p=%d pid_i=%d k_p=%d k_i=%d timestamp=%llu",
-		  __entry->tz_id, __entry->current_temp, __entry->ctrl_temp, __entry->cdev_state, __entry->pid_err_integral, __entry->pid_power_range, __entry->pid_p, __entry->pid_i, __entry->k_p, __entry->k_i, __entry->timestamp)
+	TP_printk("thermal_zone_id=%u current_temperature=%u ctrl_temp=%u cdev_state=%u pid_et_p=%d pid_power_range=%d pid_p=%d pid_i=%d k_p=%d k_i=%d timestamp=%llu",
+		  __entry->tz_id, __entry->current_temp, __entry->ctrl_temp, __entry->cdev_state, __entry->pid_et_p, __entry->pid_power_range, __entry->pid_p, __entry->pid_i, __entry->k_p, __entry->k_i, __entry->timestamp)
 );
 
 TRACE_EVENT(thermal_exynos_acpm_high_overhead,
-	TP_PROTO(int tz_id, u8 current_temp, u8 ctrl_temp, u8 cdev_state, s32 pid_err_integral, s32 k_p, s32 k_i),
-	TP_ARGS(tz_id, current_temp, ctrl_temp, cdev_state, pid_err_integral, k_p, k_i),
+	TP_PROTO(int tz_id, u8 current_temp, u8 ctrl_temp, u8 cdev_state, s32 pid_et_p, s32 k_p, s32 k_i),
+	TP_ARGS(tz_id, current_temp, ctrl_temp, cdev_state, pid_et_p, k_p, k_i),
 	TP_STRUCT__entry(
 		__field(int, tz_id)
 		__field(u8, current_temp)
 		__field(u8, ctrl_temp)
 		__field(u8, cdev_state)
-		__field(s32, pid_err_integral)
+		__field(s32, pid_et_p)
 		__field(s32, k_p)
 		__field(s32, k_i)
 	),
@@ -193,13 +193,13 @@ TRACE_EVENT(thermal_exynos_acpm_high_overhead,
 		__entry->current_temp = current_temp;
 		__entry->ctrl_temp = ctrl_temp;
 		__entry->cdev_state = cdev_state;
-		__entry->pid_err_integral = pid_err_integral;
+		__entry->pid_et_p = pid_et_p;
 		__entry->k_p = k_p;
 		__entry->k_i = k_i;
 	),
 
-	TP_printk("thermal_zone_id=%d current_temperature=%u ctrl_temp=%u cdev_state=%u pid_err_integral=%d k_p=%d k_i=%d",
-		  __entry->tz_id, __entry->current_temp, __entry->ctrl_temp, __entry->cdev_state, __entry->pid_err_integral, __entry->k_p, __entry->k_i)
+	TP_printk("thermal_zone_id=%d current_temperature=%u ctrl_temp=%u cdev_state=%u pid_et_p=%d k_p=%d k_i=%d",
+		  __entry->tz_id, __entry->current_temp, __entry->ctrl_temp, __entry->cdev_state, __entry->pid_et_p, __entry->k_p, __entry->k_i)
 );
 
 TRACE_EVENT(thermal_exynos_power_allocator_pid,
