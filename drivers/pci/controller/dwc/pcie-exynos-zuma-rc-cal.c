8a03208d23dea9f82a97f8aedbd447b39d624546
tinghsin <tinghsin@google.com>
2023-08-01 12:50:49 +0800

    pcie: exynos: Add 10us delay after turning off EXT PLL
    Bug: 287328303
    Change-Id: Ia89ac02f02748d5db5d9209087fe8ff6526f2a46
    Signed-off-by: tinghsin <tinghsin@google.com>

--- drivers/pci/controller/dwc/pcie-exynos-zuma-rc-cal.c
+++ drivers/pci/controller/dwc/pcie-exynos-zuma-rc-cal.c
@@ -110,6 +110,7 @@ void exynos_pcie_rc_phy_all_pwrdn(struct exynos_pcie *exynos_pcie, int ch_num)
 		// External PLL for PCIe PHY
 		val = readl(udbg_base_regs + 0xC700) | (0x1 << 1);
 		writel(val, udbg_base_regs + 0xC700);
+		udelay(10);
 	}
 }
 
