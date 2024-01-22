8dd0abd6a93ed3ed9a931f3e6401612ce05a8ade
Richard Chang <richardycc@google.com>
2024-01-22 03:57:45 +0000

    pixel_stat: mm: enable vh for cma_alloc latency measuring
    Replace the pixel-specific vendor hook trace events by the existed
    upstream trace events to measure the cma allocation latency:
    - trace_cma_alloc_start
    - trace_cma_alloc_finish
    
    Upstream trace events currently lack a timestamp. To address this,
    we propose extending the vendor task struct to include a timestamp
    marking the initiation of CMA allocation. The 'private' field can be
    reused without increasing the struct's size. To ensure proper usage
    and avoid stackwise conflicts, we introduce the following wrapper
    functions:
    - set_vendor_task_struct_private
    - get_and_reset_vendor_task_struct_private
    
    Bug: 315897033
    Change-Id: Ib4cd7d7a9aec1e6cb9453a38a9df6f4e9e118ffe
    Signed-off-by: Richard Chang <richardycc@google.com>
    (cherry picked from commit 0e362313b35b9d19a51c8f204df3fee5d9afca5a)
    Merged-In: Ib4cd7d7a9aec1e6cb9453a38a9df6f4e9e118ffe

--- drivers/soc/google/pixel_stat/mm/cma.h
+++ drivers/soc/google/pixel_stat/mm/cma.h
@@ -2,12 +2,15 @@
 #ifndef __MM_PIXEL_CMA_H__
 #define __MM_PIXEL_CMA_H__
 
+#include <trace/events/cma.h>
+
 struct kobject;
 
 int create_cma_sysfs(struct kobject *mm_kobj);
 void remove_cma_sysfs(void);
-void vh_cma_alloc_start(void *data, s64 *ts);
-void vh_cma_alloc_finish(void *data, struct cma *cma, struct page *page,
-			 unsigned long count, unsigned int align,
-			 gfp_t gfp_mask, s64 ts);
+void vh_cma_alloc_start(void *data, const char *name, unsigned long count,
+			unsigned int align);
+void vh_cma_alloc_finish(void *data, const char *name, unsigned long pfn,
+			 const struct page *page, unsigned long count,
+			 unsigned int align);
 #endif
