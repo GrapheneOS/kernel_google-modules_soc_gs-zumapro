f1f2f12805d2ded5d16f3f6b852fa7a4ea24ac68
Rick Yiu <rickyiu@google.com>
2024-10-10 16:08:17 +0000

    vh: sched: Protect set_prefer_idle_task_name
    Need rcu lock for for_each_process_thread.
    
    Bug: 366146314
    Change-Id: I6444ebb2972b66d9be2571c88d2207fd51856b3f
    Merged-In: I6444ebb2972b66d9be2571c88d2207fd51856b3f
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- drivers/soc/google/vh/kernel/sched/sched_lib.c
+++ drivers/soc/google/vh/kernel/sched/sched_lib.c
@@ -200,6 +200,7 @@ int set_prefer_idle_task_name(void)
 			if (tok == NULL)
 				break;
 
+			rcu_read_lock();
 			for_each_process_thread(p, t) {
 				if (strstr(t->comm, tok) != NULL) {
 					get_vendor_task_struct(t)->prefer_idle = true;
@@ -207,6 +208,7 @@ int set_prefer_idle_task_name(void)
 					break;
 				}
 			}
+			rcu_read_unlock();
 		}
 	}
 
