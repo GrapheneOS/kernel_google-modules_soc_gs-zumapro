4ad118614f8dacca5c8975b97882086d40501cc6
Rick Yiu <rickyiu@google.com>
2024-11-26 05:12:45 +0000

    vh: sched: Check if entity_is_task in check_preempt_wakeup
    Make sure the entity is task before checking preempt wakeup.
    
    Bug: 379927299
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:f10b92388a2e38f3e1dc3aab9882b11846e941c7)
    Merged-In: Ie3fadf003993180fc5194644a1669449cf14276b
    Change-Id: Ie3fadf003993180fc5194644a1669449cf14276b

--- drivers/soc/google/vh/kernel/sched/fair.c
+++ drivers/soc/google/vh/kernel/sched/fair.c
@@ -2279,6 +2279,9 @@ void rvh_check_preempt_wakeup_pixel_mod(void *data, struct rq *rq, struct task_s
 			bool *preempt, bool *nopreempt, int wake_flags, struct sched_entity *se,
 			struct sched_entity *pse, int next_buddy_marked, unsigned int granularity)
 {
+	if (!entity_is_task(se) || !entity_is_task(pse))
+		return;
+
 	/*
 	 * Let ADPF task preempt non-ADPF task.
 	 */
