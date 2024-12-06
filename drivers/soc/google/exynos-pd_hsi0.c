e64201e95b117be1cc54563047bb953a8be539be
Ray Chi <raychi@google.com>
2024-12-06 16:22:21 +0800

    Revert "soc: google: add mutex lock to protect ldo status"
    This reverts commit 08226ab8f62de608924aa5a31e5a04c1ec8e833b.
    
    Reason for revert: cause MTBF drop
    
    Bug: 367836656
    Signed-off-by: Ray Chi <raychi@google.com>
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:2604afd4bb24e49c8f76ef9924b95ca23ca6bee0)
    Merged-In: I2913e6844c6d37c72a1096d96ad84de4d22fe43d
    Change-Id: I2913e6844c6d37c72a1096d96ad84de4d22fe43d

--- drivers/soc/google/exynos-pd_hsi0.c
+++ drivers/soc/google/exynos-pd_hsi0.c
@@ -97,16 +97,13 @@ int exynos_pd_hsi0_ldo_manual_control(bool on)
 {
 	struct exynos_pd_hsi0_data *hsi0_data;
 
+	pr_info("%s ldo = %d\n", __func__, on);
+
 	hsi0_data = exynos_pd_hsi0_get_struct();
 	if (!hsi0_data)
 		return -ENODEV;
 
-	mutex_lock(&hsi0_data->power_lock);
-
-	pr_info("%s ldo = %d\n", __func__, on);
 	exynos_pd_hsi0_ldo_control(hsi0_data, on);
-
-	mutex_unlock(&hsi0_data->power_lock);
 
 	eusb_repeater_update_usb_state(on);
 
@@ -146,34 +143,6 @@ bool exynos_pd_hsi0_get_ldo_status(void)
 	return false;
 }
 EXPORT_SYMBOL_GPL(exynos_pd_hsi0_get_ldo_status);
-
-void exynos_pd_hsi0_write_lock(void)
-{
-	struct exynos_pd_hsi0_data *hsi0_data;
-
-	hsi0_data = exynos_pd_hsi0_get_struct();
-	if (!hsi0_data)
-		return;
-
-	mutex_lock(&hsi0_data->power_lock);
-
-	return;
-}
-EXPORT_SYMBOL_GPL(exynos_pd_hsi0_write_lock);
-
-void exynos_pd_hsi0_write_unlock(void)
-{
-	struct exynos_pd_hsi0_data *hsi0_data;
-
-	hsi0_data = exynos_pd_hsi0_get_struct();
-	if (!hsi0_data)
-		return;
-
-	mutex_unlock(&hsi0_data->power_lock);
-
-	return;
-}
-EXPORT_SYMBOL_GPL(exynos_pd_hsi0_write_unlock);
 
 static int exynos_pd_hsi0_probe(struct platform_device *pdev)
 {
@@ -226,8 +195,6 @@ static int exynos_pd_hsi0_probe(struct platform_device *pdev)
 		dev_err(dev, "get vdd_low regulator failed: %ld\n", PTR_ERR(hsi0_data->vdd_low));
 		return PTR_ERR(hsi0_data->vdd_low);
 	}
-
-	mutex_init(&hsi0_data->power_lock);
 
 	/* vote on due to the regulators already turned on */
 #if IS_ENABLED(CONFIG_SOC_GS101) || IS_ENABLED(CONFIG_SOC_GS201)
